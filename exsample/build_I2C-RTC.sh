ccb11f2003ab917e097adaed8ad670c3833746d3
kuninet <kuninet@gmail.com>
2020-03-08 22:04:32 +0900

    ソース整理、DS1307対応

--- exsample/build_I2C-RTC.sh
+++ exsample/build_I2C-RTC.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -eu
 
-asl -cpu z80 -L -D PIO -OLIST I2C-DS3231_WR.lst -o I2C-DS3231_WR.p I2C-DS3231_WR.ASM
+asl -cpu z80 -L -D DS3231 -OLIST I2C-DS3231_WR.lst -o I2C-DS3231_WR.p I2C-RTC_WR.ASM
 p2hex -r \$-\$ -F Intel I2C-DS3231_WR.p
 
-asl -cpu z80 -L -D PIO -OLIST I2C-DS3231_RD.lst -o I2C-DS3231_RD.p I2C-DS3231_RD.ASM
+asl -cpu z80 -L -D DS3231 -OLIST I2C-DS3231_RD.lst -o I2C-DS3231_RD.p I2C-RTC_RD.ASM
 p2hex -r \$-\$ -F Intel I2C-DS3231_RD.p
+
+asl -cpu z80 -L -D DS1307 -OLIST I2C-DS1307_WR.lst -o I2C-DS1307_WR.p I2C-RTC_WR.ASM
+p2hex -r \$-\$ -F Intel I2C-DS1307_WR.p
+
+asl -cpu z80 -L -D DS1307 -OLIST I2C-DS1307_RD.lst -o I2C-DS1307_RD.p I2C-RTC_RD.ASM
+p2hex -r \$-\$ -F Intel I2C-DS1307_RD.p
