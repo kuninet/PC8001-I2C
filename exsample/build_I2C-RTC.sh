006e71279caafa2fe9c3a94deaeef8f9e05b9028
kuninet <kuninet@gmail.com>
2020-03-13 22:01:01 +0900

    サンブル機械語サブルーチンをBASICからCallする形で整備

--- exsample/build_I2C-RTC.sh
+++ exsample/build_I2C-RTC.sh
@@ -1,13 +1,18 @@
 #!/bin/bash -eu
+rm -f *.hex *.lst *.p *.bin
 
-asl -cpu z80 -L -D DS3231 -OLIST I2C-DS3231_WR.lst -o I2C-DS3231_WR.p I2C-RTC_WR.ASM
-p2hex -r \$-\$ -F Intel I2C-DS3231_WR.p
+asl -cpu z80 -L -D DS3231 -OLIST DS3231WR.lst -o DS3231WR.p I2C-RTC_WR.ASM
+p2hex -r \$-\$ -F Intel DS3231WR.p
+p2bin -r \$-\$ DS3231WR.p
 
-asl -cpu z80 -L -D DS3231 -OLIST I2C-DS3231_RD.lst -o I2C-DS3231_RD.p I2C-RTC_RD.ASM
-p2hex -r \$-\$ -F Intel I2C-DS3231_RD.p
+asl -cpu z80 -L -D DS3231 -OLIST DS3231RD.lst -o DS3231RD.p I2C-RTC_RD.ASM
+p2hex -r \$-\$ -F Intel DS3231RD.p
+p2bin -r \$-\$ DS3231RD.p
 
-asl -cpu z80 -L -D DS1307 -OLIST I2C-DS1307_WR.lst -o I2C-DS1307_WR.p I2C-RTC_WR.ASM
-p2hex -r \$-\$ -F Intel I2C-DS1307_WR.p
+asl -cpu z80 -L -D DS1307 -OLIST DS1307WR.lst -o DS1307WR.p I2C-RTC_WR.ASM
+p2hex -r \$-\$ -F Intel DS1307WR.p
+p2bin -r \$-\$ DS1307WR.p
 
-asl -cpu z80 -L -D DS1307 -OLIST I2C-DS1307_RD.lst -o I2C-DS1307_RD.p I2C-RTC_RD.ASM
-p2hex -r \$-\$ -F Intel I2C-DS1307_RD.p
+asl -cpu z80 -L -D DS1307 -OLIST DS1307RD.lst -o DS1307RD.p I2C-RTC_RD.ASM
+p2hex -r \$-\$ -F Intel DS1307RD.p
+p2bin -r \$-\$ DS1307RD.p
